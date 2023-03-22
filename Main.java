3387c209cbf7a83a0df64d09ca96643048e7ade3
Wilkqs <127328280+Wilkqs@users.noreply.github.com>
2023-03-22 15:19:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+  
+    String[] hello = new String[10];
+    hello[0] = "h";
+    hello[1] = "e";
+    hello[2] = "l";
+    hello[3] = "l";
+    hello[4] = "o";
+    hello[5] = "w";
+    hello[6] = "o";
+    hello[7] = "r";
+    hello[8] = "l";
+    hello[9] = "d";
+    for (int i = 0; i <10 ; i++){
+      System.out.println(hello[i]);
+    
+  }
+  }
+}
